8cbe44a898ed6c3e8a303e76f9117b5084b6c8b2
Rahul Chinta <rahul.25bce7302@vitapstudent.ac.in>
2026-08-01 23:17:07 +0530

    Time: 3 ms (86.05%), Space: 43 MB (86.19%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        int n = s.length();
+        for(int i = 0;i<n;i++){
+            char c = s.charAt(i);
+            if(c == '('){
+                stack.push(')');
+            }else if(c == '{'){
+                stack.push('}');
+            }else if(c =='['){
+                stack.push(']');
+            }else if (stack.isEmpty()||stack.pop()!=c){
+                return false;
+            }
+
+        }
+        return stack.isEmpty();
+    }
+}
